02d26d5cbb067621cbd935345560ea817bf3e463
DoNdRa123 <159123607+DoNdRa123@users.noreply.github.com>
2025-01-20 10:13:03 +0000

    add_row

--- soubor.py
+++ soubor.py
@@ -1,8 +1,9 @@
-.#dvourozměrného pole
+.#dvourozměrné pole
 dvourozmerne_pole = [
     [1, 2, 3],
     [4, 105, 6],
-    [7, 8, 9]
+    [7, 8, 9],
+    [10, 11, 12]
 ]
 for nevim in dvourozmerne_pole:
     print(nevim)
